84930a25703218f8f465e5c27ce06047f676edef
MattHackenbrook <77515289+MattHackenbrook@users.noreply.github.com>
2023-02-25 21:00:59 -0500

    Final Lab 4 version

--- src/IndexController.js
+++ src/IndexController.js
@@ -1,36 +1,60 @@
-$(document).ready(function(){
-    var ImageArr = new Array("BlondeWoman.jpg",
-    "BrunetteWomanWithEarrings.jpg",
-    "BusinessWoman.jpg",
-    "ManOnBoatWithApe.jpg",
-    "Professor.jpg");
+$(function(){
+
+    var ImageLocs = new Array();
     var Index = 0;
+
+    function SpaceOnCaps(str){
+        str = str.replaceAll("A", " a");
+        str = str.replaceAll("B", " b");
+        str = str.replaceAll("C", " c");
+        str = str.replaceAll("D", " d");
+        str = str.replaceAll("E", " e");
+        str = str.replaceAll("F", " f");
+        str = str.replaceAll("G", " g");
+        str = str.replaceAll("H", " h");
+        str = str.replaceAll("I", " i");
+        str = str.replaceAll("J", " j");
+        str = str.replaceAll("K", " k");
+        str = str.replaceAll("L", " l");
+        str = str.replaceAll("M", " m");
+        str = str.replaceAll("N", " n");
+        str = str.replaceAll("O", " o");
+        str = str.replaceAll("P", " p");
+        str = str.replaceAll("Q", " q");
+        str = str.replaceAll("R", " r");
+        str = str.replaceAll("S", " s");
+        str = str.replaceAll("T", " t");
+        str = str.replaceAll("U", " u");
+        str = str.replaceAll("V", " v");
+        str = str.replaceAll("W", " w");
+        str = str.replaceAll("X", " x");
+        str = str.replaceAll("Y", " y");
+        str = str.replaceAll("Z", " z");
+        return str;
+    }
+    
 
     function CycleImage(){
         var imgVal = $("#FakePeople").find("img");
-        // Make an AJAX call to get the image file
-        $.ajax({
-            url: "https://github.com/MattHackenbrook/MattHackenbrook.github.io/tree/main/FakePeople/" + ImageArr[Index],
-            dataType: "binary",
-            responseType: "arraybuffer",
-            success: function (data) {
-                const base64Image = btoa(String.fromCharCode.apply(null, new Uint8Array(data)));
-
-                imgVal.attr("src", "data:image/jpeg;base64," + base64Image);
-            },
-            error: function (xhr, status, error) {
-            console.log("Error: " + error);
-            }
-        });
-  
-        //imgVal.attr("src", "C:\\Users\\Matthew\\Desktop\\CCPS530_Lab4\\FakePeople\\" + ImageArr[Index]);
-        
         var imgCap = $("#FakePeople").find("figcaption");
-        imgCap.text(ImageArr[Index]);
+        imgVal.attr("src", ImageLocs[Index]);
 
-        Index = (Index + 1) % ImageArr.length ;
+        var urlComponents = ImageLocs[Index].split('/');
+        var capText = "An image of a" + SpaceOnCaps(urlComponents[urlComponents.length - 1].split('.')[0]) + " who does not exist";
+        imgCap.text(capText);
+
+        Index = (Index + 1) % ImageLocs.length;
     }
 
-    CycleImage()
-    setInterval(CycleImage, 2000);
+    $.get("FakePeople", function(data, status){
+        $(data).find("a").each(function(){
+            var fileName = $(this).attr("href");
+            if(fileName != "/"){
+                ImageLocs.push(fileName);
+            }
+        });
+    }).done(function(){
+        CycleImage();
+        setInterval(CycleImage, 20000);
+    });
 });
